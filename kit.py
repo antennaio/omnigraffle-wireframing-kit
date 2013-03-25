06409e87baf09ac98016bf506a8dd48fa6dd53d8
netboy <netboy@netboy.pl>
2013-03-25 19:22:53 +0100

    core functionality moved to kit.py

---
+++ kit.py
@@ -0,0 +1,63 @@
+class Link(object):
+    """Generic link."""
+
+    def __init__(self, id, page, title, device, orientation):
+        self.id = id
+        self.page = page
+        self.title = title
+        self.device = device
+        self.orientation = orientation
+
+    def url(self):
+        """Returns an URL."""
+        return str(self.page) + '-' + self.device + '-' + self.orientation + '.html'
+
+
+class LinkSet(object):
+    """A set of links."""
+
+    def __init__(self):
+        self.links = []
+
+    def __iter__(self):
+        return iter(self.links)
+
+    active_id = 0
+
+
+class WireframeSet(LinkSet):
+    """A set of wireframes."""
+
+    def __init__(self, config):
+        super(WireframeSet, self).__init__()
+
+        self.config = config
+
+        for c in xrange(0, len(self.config['Wireframes'])):
+            wireframe_id = c + 1
+            w = self.config['Wireframes']['Wireframe ' + str(wireframe_id)]
+            d = self.config['Devices']['Device ' + w['show_on_device']]
+            self.links.append(Link(wireframe_id, wireframe_id, w['title'], d['device'], d['orientation']))
+
+
+class DeviceSet(LinkSet):
+    """A set of devices."""
+
+    def __init__(self, config):
+        super(DeviceSet, self).__init__()
+
+        self.config = config
+
+        for c in xrange(0, len(self.config['Devices'])):
+            device_id = c + 1
+            d = self.config['Devices']['Device ' + str(device_id)]
+            self.links.append(Link(device_id, None, d['title'], d['device'], d['orientation']))
+
+    def get_id(self, device, orientation):
+        for d in self.links:
+            if d.device == device and d.orientation == orientation:
+                return d.id
+
+    def set_page(self, page):
+        for d in self.links:
+            d.page = page
--- launch.py
+++ launch.py
@@ -14,10 +14,13 @@ except (ImportError):
 import re
 import os.path
 
+from kit import WireframeSet, DeviceSet
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 omnigraffle_folder = 'omnigraffle'
+
 config_file = 'wireframes.ini'
 config_spec = 'wireframes.spec'
 
@@ -35,91 +38,40 @@ if not result:
     exit(1)
 
 
-def format_filename(device, orientation, id='1'):
-    """Format a file name."""
-    return id + '-' + device + '-' + orientation + '.html'
-
-
-def wireframes(active_id=0):
-    """Make a list of dictionaries with all wireframes."""
-    wireframes = []
-
-    for c in xrange(0, len(config['Wireframes'])):
-        wireframe_id = c + 1
-        w = config['Wireframes']['Wireframe ' + str(wireframe_id)]
-        show_on_device = w['show_on_device']
-        d = config['Devices']['Device ' + str(show_on_device)]
-
-        data = {
-            'title': w['title'],
-            'url': format_filename(d['device'], d['orientation'], str(wireframe_id))
-        }
-
-        if active_id == wireframe_id:
-            data['active'] = True
-
-        wireframes.append(data)
-
-    return wireframes
-
-
 @app.context_processor
 def project_title():
     """Project title available in templates."""
     return dict(project_title=config['General']['project'])
 
 
-@app.context_processor
-def initial_wireframe():
-    """Initial wireframe available in templates."""
-    w = config['Wireframes']['Wireframe 1']
-    show_on_device = w['show_on_device']
-    d = config['Devices']['Device ' + str(show_on_device)]
-    return dict(initial_wireframe=format_filename(device=d['device'], orientation=d['orientation']))
-
-
 @app.route('/', methods=['GET'])
 def home():
-    return render_template('home.html', wireframes=wireframes())
+    return render_template('home.html', wireframes=WireframeSet(config))
 
 
 @app.route('/<int:wireframe_id>-<string:device>-<string:orientation>.html', methods=['GET'])
 def show(wireframe_id, device, orientation):
+
     # open wireframe exported from Omnigraffle
     path = 'static' + os.sep + omnigraffle_folder + os.sep + device + '-' + orientation
-    wireframe_filename = format_filename(device, orientation, str(wireframe_id))
+    filename = str(wireframe_id) + '-' + device + '-' + orientation + '.html'
 
     try:
-        f = open(os.path.join(path, wireframe_filename), "r")
+        f = open(os.path.join(path, filename), "r")
     except IOError:
-        return render_template('not_found.html', wireframes=wireframes())
+        return render_template('not_found.html', wireframes=WireframeSet(config))
 
     # fix img src
-    file_content = re.sub(r'src=\"', 'src="%s/' % path, f.read())
+    wireframe = re.sub(r'src=\"', 'src="%s/' % path, f.read())
 
+    devices = DeviceSet(config)
+    devices.set_page(wireframe_id)
+    devices.active_id = devices.get_id(device, orientation)
 
-    # make a list of dictionaries with devices on which this wireframe will be displayed
-    devices = []
+    wireframes = WireframeSet(config)
+    wireframes.active_id = wireframe_id
 
-    for c in xrange(0, len(config['Devices'])):
-        device_id = c + 1
-        d = config['Devices']['Device ' + str(device_id)]
-
-        path = 'static' + os.sep + omnigraffle_folder + os.sep + d['device'] + '-' + d['orientation']
-        device_filename = format_filename(d['device'], d['orientation'], str(wireframe_id))
-
-        if os.path.isfile(os.path.join(path, device_filename)):
-            data = {
-                'title': d['title'],
-                'url': device_filename
-            }
-
-            if device_filename == wireframe_filename:
-                data['active'] = True
-
-            devices.append(data)
-
-    return render_template('show.html', wireframes=wireframes(wireframe_id), wireframe=file_content, devices=devices)
+    return render_template('show.html', wireframes=wireframes, devices=devices, wireframe=wireframe)
 
 
 if __name__ == '__main__':
