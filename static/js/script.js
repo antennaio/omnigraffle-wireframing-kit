2e84b0bf8dcec46ffbe429dc11cc9e415db4a9e8
netboy <netboy@netboy.pl>
2013-03-27 11:47:36 +0100

    improved sidebar and color scheme

--- static/js/script.js
+++ static/js/script.js
@@ -1,29 +1,58 @@
 $(window).load(function() {
-	var i = 0;
 
-	function fixHeight(selector) {
-		h = $(document).height() - 50;
-		$(selector).css({'height': h + 'px'});
+	function fixSidebar() {
+		h = $(window).height() - $('#header').height();
+
+		$('#dragbar, #sidebar .menu')
+			.css({'height': h + 'px'});
+
+		if ($('#sidebar .menu ul').height() > ($(window).height() - $('#header').height())) {
+			$('#sidebar .menu')
+				.css('overflow-y', 'scroll');
+		} else {
+			$('#sidebar .menu')
+				.css('overflow-y', 'auto');
+		}
+
+		$('#dragbar')
+			.css('background-position', 'center ' + (($(window).height() / 2) - 40) + 'px');
+
 	}
 
-	fixHeight('#dragbar, #sidebar'); 
+	fixSidebar();
+
+	if (($.cookie('sidebar_hidden') === 'true') || (!$.cookie('sidebar_hidden'))) {
+		$('#sidebar')
+			.css('left', '-' + ($('#sidebar').width() - $('#dragbar').width()) + 'px')
+			.data('hidden', true)
+			.show();
+
+		$('#dragbar').addClass('dragbar-hidden');
+
+	} else {
+		$('#sidebar').show();
+		$('#dragbar').removeClass('dragbar-hidden');
+	}
 
 	$('#dragbar').mousedown(function(e) {
 		e.preventDefault();
-		$(document).mousemove(function(e) {
-			if ((e.pageX > 150) && (e.pageX < 600)) {
-				$('#sidebar').css("width", e.pageX + 2);
-				$('#main').css("left", e.pageX + 2);
-				fixHeight('#dragbar, #sidebar');
-			}
-		})
+		if ($('#sidebar').data('hidden') === true) {
+			$('#sidebar').css('left', '0').data('hidden', false);
+			$('#dragbar').removeClass('dragbar-hidden');
+			$.cookie('sidebar_hidden', 'false');
+		} else {
+			$('#sidebar')
+				.css('left', '-' + ($('#sidebar').width() - $('#dragbar').width()) + 'px')
+				.data('hidden', true);
+
+			$('#dragbar').addClass('dragbar-hidden');
+			$.cookie('sidebar_hidden', 'true');
+
+		}
 	});
 
-	$(document).mouseup(function(e) {
-		$(document).unbind('mousemove');
-	});
-
-    $(window).on("debouncedresize", function(e) {
-        fixHeight('#dragbar, #sidebar');
+    $(window).on('debouncedresize', function(e) {
+        fixSidebar();
     });
+
 });
